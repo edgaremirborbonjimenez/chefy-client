aa0a175244593e93e9c8356596cc92e249b72896
SnakeEater007 <edemboji@gmail.com>
2023-09-30 09:48:01 -0700

    Initial commit

---
+++ src/app/folder/folder.page.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { FolderPage } from './folder.page';
+
+describe('FolderPage', () => {
+  let component: FolderPage;
+  let fixture: ComponentFixture<FolderPage>;
+
+  beforeEach(async () => {
+    TestBed.overrideComponent(FolderPage, {
+      add: {
+        imports: [RouterTestingModule]
+      }
+    });
+
+    fixture = TestBed.createComponent(FolderPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
